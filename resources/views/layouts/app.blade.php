1d840bcda994bbe33425c8fb0ecbee69dc351145
lessandsass <lessandsass@gmail.com>
2023-11-07 17:30:53 +0530

    1. home routes was created 2. home.blade.php view was created. 3. app.blade.php was created inside the layouts folder.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Authentication</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-400">
+
+    <nav class="p-6 bg-gray-800 flex justify-between">
+        <ul>
+            <a href="">Home</a>
+        </ul>
+
+        <ul>
+
+        </ul>
+    </nav>
+
+    @yield('content')
+</body>
+</html>
