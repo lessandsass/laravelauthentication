6fa963aa8a46edc5c0439fc4a2f69a53c1938a18
lessandsass <lessandsass@gmail.com>
2023-11-08 21:20:58 +0530

    some tailwind styles added.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Authentication</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="bg-gray-400">
 
-    <nav class="p-6 bg-gray-800 flex justify-between">
-        <ul>
-            <a href="">Home</a>
+    <nav class="p-6 bg-gray-800 text-gray-200 flex justify-between">
+        <ul class="flex items-center">
+            <a href="" class="p-3">Home</a>
         </ul>
 
-        <ul>
-
+        <ul class="flex items-center">
+            <a href="" class="p-3">About</a>
         </ul>
     </nav>
 
