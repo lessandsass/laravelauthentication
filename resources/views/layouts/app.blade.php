936c9d43d4c221f629637570b4b8b2c58513eb0e
lessandsass <lessandsass@gmail.com>
2023-11-11 07:09:33 +0530

    the user was authorized to be logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,25 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Authentication</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
-<body class="bg-gray-400">
+<body class="bg-gray-900">
 
-    <nav class="p-6 bg-gray-800 text-gray-200 flex justify-between">
+    <nav class="p-6 bg-gray-800 text-gray-300 flex justify-between">
         <ul class="flex items-center">
-            <a href="" class="p-3">Home</a>
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
         </ul>
 
         <ul class="flex items-center">
-            <a href="" class="p-3">About</a>
+
+            @auth
+                <li><a href="" class="p-3">{{ auth()->user()->name }}</a></li>
+                <li><a href="" class="p-3">Logout</a></li>
+            @endauth
+
+            @guest
+                <li><a href="" class="p-3">Login</a></li>
+                <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
+
         </ul>
     </nav>
 
-    @yield('content')
+    <div class="container mx-auto mt-6 px-6">
+        @yield('content')
+    </div>
 </body>
 </html>
