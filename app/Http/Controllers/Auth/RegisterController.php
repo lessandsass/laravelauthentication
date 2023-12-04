c2dba0c55607f9021ac265bbc7e8ff6f8d68f0cb
lessandsass <lessandsass@gmail.com>
2023-12-04 11:28:06 +0530

    guest middleware was added to LoginController and RegisterController

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+
+class RegisterController extends Controller
+{
+    public function index() :View
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request) :RedirectResponse
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect('dashboard');
+    }
+}
